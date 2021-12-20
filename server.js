67dad80d1f3b5860c1865776935d6fd2cc88eebf
eduardoperetto <erperetto@gmail.com>
2021-12-20 14:41:55 -0300

    Finishing project API

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 // server.js
 // where your node app starts
 
+/* A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+
+A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+
+A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+Your project can handle dates that can be successfully parsed by new Date(date_string)
+
+If the input date string is invalid, the api returns an object having the structure { error : "Invalid Date" }
+
+An empty date parameter should return the current time in a JSON object with a unix key
+
+An empty date parameter should return the current time in a JSON object with a utc key */
+
 // init project
 var express = require('express');
 var app = express();
@@ -8,7 +22,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +32,31 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  let currentDate = new Date();
+  res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
 });
 
+app.get("/api/:date", function (req, res) {
+  let receivedDate = req.params.date;
+  if (/\D/.test(receivedDate) === false)
+    receivedDate = parseInt(receivedDate);
 
+  function isDateValid(date) {
+    if (new Date(date) != "Invalid Date")
+      return true
+    else return false
+  }
+
+  if (isDateValid(receivedDate)) {
+    let receivedToDate = new Date(receivedDate);
+    res.json({ unix: receivedToDate.getTime(), utc: receivedToDate.toUTCString() });
+  }
+  else res.json({ error: "Invalid Date" });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
